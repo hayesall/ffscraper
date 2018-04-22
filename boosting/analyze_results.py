7a20ec1564ea3112027ffe39401fa1ec5def8607
batflyer <alexander@batflyer.net>
2018-04-22 15:34:22 -0500

    Adding metadata and a description snippet.

---
+++ boosting/analyze_results.py
@@ -0,0 +1,35 @@
+
+#   Copyright (c) 2018 Alexander L. Hayes (@batflyer)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from __future__ import print_function
+
+import argparse
+import re
+
+"""
+A short Python script for analyzing the results output from
+run_boosting_jobs.sh
+"""
+
+__author__ = 'Alexander L. Hayes (@batflyer)'
+__copyright__ = 'Copyright (c) 2018 Alexander L. Hayes'
+__license__ = 'Apache'
+__version__ = '0.0.1'
+__maintainer__ = __author__
+__email__ = 'alexander@batflyer.net'
+__status__ = 'Prototype'
+
+if  __name__ == '__main__':
+    pass
