529449792341e7168e89da2a6d9b29496786885b
batflyer <alexander@batflyer.net>
2018-04-24 13:46:09 -0500

    Separating some functions out into their own files (too much scrolling).

--- scraper/FanfictionScraper.py
+++ scraper/FanfictionScraper.py
@@ -107,66 +107,6 @@ def FanfictionScraper(storyid, rate_limit=3):
     #print(soup.prettify())
     return story
 
-def ReviewScraper(storyid, reviews_num, rate_limit=3):
-    """
-    Scrapes the reviews for a certain story.
-
-    @method ReviewScraper
-    @param  {str}               storyid         The id for a particular story.
-    @param  {int}               reviews_num     The number of reviews in metadata
-    @param  {int}               rate_limit      rate limit (in seconds)
-    @return {}
-
-    Discussion:
-        * Reviews on FanFiction.Net may either be anonymous or tied to the user
-          who left the review.
-        * Reviews for a story are located at address:
-          https://www.fanfiction.net/r/[sid]/0/1/
-        * /0/ represents all reviews for a story, /1/ is a page, and there are
-          up to 15 reviews per page.
-        * Incrementing the 0 gives the reviews for a particular chapter.
-
-    Page Layout:
-        * Reviews are stored in an html table of up to 15 elements.
-        * A review may be thought of as a 4-tuple:
-            (userid, chapter_reviewed, date, review_text)
-    """
-
-    # There may be up to 15 reviews on a single page, therefore the number of
-    # pages the reviews  are stored on is equal to the following:
-    number_of_pages = (reviews_num // 15) + 1
-
-    for p in range(number_of_pages):
-
-        # Rate limit
-        time.sleep(rate_limit)
-
-        r = requests.get('https://www.fanfiction.net/r/' + storyid + '/0/' + str(p+1) + '/')
-        html = r.text
-        soup = bs(html, 'html.parser')
-
-        # Get the tbody, which is where the review table is stored
-        t = soup.find('tbody')
-
-        # Loop over the table entries (td)
-        for review in t.find_all('td'):
-
-            # Reviews link to the profile of the user who reviewed it.
-            for link in review.find_all('a', href=True):
-
-                if '/u/' in str(link):
-                    print(str(link).split('"')[1].split('/')[2])
-                print()
-
-            print(review.text)
-            #exit()
-            time.sleep(0.5)
-
-
-        #exit()
-
-        print(p+1)
-
 def PredicateLogicBuilder(type, id, value):
     """
     Converts inputs into (id, value) pairs, creating positive examples
