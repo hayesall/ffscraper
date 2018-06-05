873b2d87feed154ddfc7806036544871f79b5a48
batflyer <alexander@batflyer.net>
2018-06-05 11:40:36 -0500

    Incremental progress on the database design.

--- mysql/fanfiction.sql
+++ mysql/fanfiction.sql
@@ -10,6 +10,33 @@ create table story(
   title char(100) not null
 );
 
+create table word(
+  wordid int unsigned not null auto_increment primary key,
+  string char(100) not null,
+  stem char(100),
+  lemma char(100)
+);
+
+create table profile(
+  userid int unsigned not null,
+  content text,
+  stamp timestamp(12),
+
+  foreign key(userid) references user(userid)
+);
+
+create table review(
+  userid int unsigned not null,
+  reviewid int unsigned not null auto_increment,
+  polarity float,
+  subjectivity float,
+  stamp timestamp(12),
+  content text,
+
+  foreign key(userid) references user(userid),
+  primary key(userid, reviewid)
+);
+
 /* Tables for relations derived from the basic types */
 create table author(
 
